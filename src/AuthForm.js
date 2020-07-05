633dcfc10569c2f36a16bde60348683578667ade
Chirag Gupta <gchirag@apache.org>
2020-07-05 23:54:32 +0530

    Authentication HTTP request setup

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,36 +1,58 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Alert, Container } from "react-bootstrap";
 import Axios from "axios";
+import { navigate } from "@reach/router";
 
 const AuthForm = () => {
+  const [loginError, setLoginError] = useState(false);
+
   return (
-    <Form
-      onSubmit={({ value }) => {
-        event.preventDefault();
-        Axios.post("some-url", value).then((response) => {
-          // If response is good then route to next page (add Reach Router)
-          // If response is error, then show a snackbar saying that either username or password is incorrect.
-        });
-      }}
-    >
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control placeholder="Enter username" />
-      </Form.Group>
+    <Container>
+      {loginError ? (
+        <Alert variant="danger">
+          Either username or password is incorrect!
+        </Alert>
+      ) : null}
+      <Form
+        onSubmit={(value) => {
+          event.preventDefault();
+          Axios.get("https://api.bitbucket.org/2.0/repositories/hmg65", {
+            auth: {
+              username: value.formUsername,
+              passowrd: value.formPassword,
+            },
+          })
+            .then((response) => {
+              if (response.status == 200) {
+                navigate("/dashboard");
+              }
+            })
+            .catch((error) => {
+              if (error.response.status == 401) {
+                setLoginError(true);
+              }
+            });
+        }}
+      >
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control placeholder="Enter username" />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-      <Form.Group controlId="formRememberMe">
-        <Form.Check type="checkbox" label="Remember me" />
-      </Form.Group>
+        <Form.Group controlId="formRememberMe">
+          <Form.Check type="checkbox" label="Remember me" />
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
