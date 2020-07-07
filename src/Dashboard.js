86e170be347d05a73be52840bacf332bf2c53b5c
Chirag Gupta <gchirag@apache.org>
2020-07-08 01:50:07 +0530

    fetch all PR related data in DevReport

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,28 +4,27 @@ import Axios from 'axios';
 import { navigate } from '@reach/router';
 
 const Dashboard = (props) => {
-	const [ users, setUsers ] = useState([]);
-	const getUsers = async () => {
-		await Axios.get('https://api.bitbucket.org/2.0/workspaces/hmg65/members', {
-			auth: {
-				username: props.location.state.username,
-				password: props.location.state.password
-			}
-		}).then((response) => {
-			if (response.status === 200) {
-				const temp_users = [];
-				response.data.values.map((u) => {
-					temp_users.push({
-						id: u.user.uuid,
-						img_link: u.user.links.avatar.href,
-						name: u.user.nickname
-					});
-				});
-				setUsers(temp_users);
-				console.log(users);
-			}
-		});
-	};
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    await Axios.get("https://api.bitbucket.org/2.0/workspaces/hmg65/members", {
+      auth: {
+        username: props.location.state.username,
+        password: props.location.state.password,
+      },
+    }).then((response) => {
+      if (response.status === 200) {
+        const temp_users = [];
+        response.data.values.map((u) => {
+          temp_users.push({
+            id: u.user.uuid,
+            img_link: u.user.links.avatar.href,
+            name: u.user.nickname,
+          });
+        });
+        setUsers(temp_users);
+      }
+    });
+  };
 
 	useEffect(() => {
 		getUsers();
--- src/DevReport.js
+++ src/DevReport.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
-import Axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import Axios from "axios";
 
 const DevReport = (props) => {
 	const start = new Date();
@@ -9,10 +9,10 @@ const DevReport = (props) => {
 	var end = new Date();
 	end.setHours(23, 59, 59, 999);
 
-	const [ pullsOpened, setPullsOpened ] = useState([]);
-	const [ pullsMerged, setPullsMerged ] = useState([]);
-	const [ pullsComments, setPullsComments ] = useState([]);
-	const [ pullsUpdated, setPullsUpdated ] = useState([]);
+  const [pullsOpened, setPullsOpened] = useState([]);
+  const [pullsMerged, setPullsMerged] = useState([]);
+  const [pullsComments, setPullsComments] = useState([]);
+  const [pullsUpdated, setPullsUpdated] = useState([]);
 
 	const issuesOpened = [];
 	const issueComments = [];
@@ -21,88 +21,53 @@ const DevReport = (props) => {
 	const commitsCreated = [];
 	const commitComments = [];
 
-	const getOpenPulls = async () => {
-		await Axios.get(
-			`https://api.bitbucket.org/2.0/repositories/hmg65/sia-lounge-backend/pullrequests?q=(created_on>=${start.toISOString()} AND created_on<=${end.toISOString()})`,
-			{
-				auth: {
-					username: props.location.state.username,
-					password: props.location.state.password
-				}
-			}
-		).then((response) => {
-			const tempPullsOpened = [];
-			response.data.values.map((pull) => {
-				if (pull.author.uuid === props.location.state.id) {
-					tempPullsOpened.push({
-						title: pull.title,
-						link: pull.links.html.href
-					});
-				}
-			});
-			setPullsOpened(tempPullsOpened);
-		});
-	};
 
-	const getUpdatedPulls = async () => {
-		await Axios.get(
-			`https://api.bitbucket.org/2.0/repositories/hmg65/sia-lounge-backend/pullrequests?q=(updated_on>=${start.toISOString()} AND updated_on<=${end.toISOString()})`,
-			{
-				auth: {
-					username: props.location.state.username,
-					password: props.location.state.password
-				}
-			}
-		).then((response) => {
-			const tempPullsUpdated = [],
-				tempPullsMerged = [],
-				tempPullsComments = [];
-			response.data.values.map((pull) => {
-				if (pull.author.uuid === props.location.state.id) {
-					if (pull.state === 'MERGED') {
-						tempPullsMerged.push({
-							title: pull.title,
-							link: pull.links.html.href
-						});
-					} else {
-						if (start >= pull.created_on) {
-							tempPullsUpdated.push({
-								title: pull.title,
-								link: pull.links.html.href
-							});
-						}
-					}
-				} else {
-					// Check PR if he has commented on it
-					Axios.get('url-to-be-placed', {
-						auth: {
-							username: props.location.state.username,
-							password: props.location.state.password
-						}
-					}).then((response) => {
-						console.log(response.data);
-					});
-				}
-			});
-			setPullsUpdated(tempPullsUpdated);
-			setPullsMerged(tempPullsMerged);
-			setPullsComments(tempPullsComments);
-		});
-	};
+  useEffect(() => {
+    Axios.get(
+      `https://api.bitbucket.org/2.0/repositories/hmg65/sia-lounge-backend/pullrequests?q=(created_on>=${start.toISOString()} AND created_on<=${end.toISOString()})`,
+      {
+        auth: {
+          username: "hmg65",
+          password: "code@123",
+        },
+      }
+    ).then((response) => {
+      // Get PRs which are created by the dev today
+      response.data.values.map((pull) => {
+        console.log(pull);
+      });
+    });
 
-	useEffect(() => {
-		getOpenPulls();
-		getUpdatedPulls();
-	}, []);
+    Axios.get(
+      `https://api.bitbucket.org/2.0/repositories/hmg65/sia-lounge-backend/pullrequests?q=(updated_on>=${start.toISOString()} AND updated_on<=${end.toISOString()})`,
+      {
+        auth: {
+          username: "hmg65",
+          password: "code@123",
+        },
+      }
+    ).then((response) => {
+      // Check each PR manually to check dev's comment
+      response.data.values.map((pull) => {
+        console.log(pull);
+      });
+    });
+  });
 
-	return (
-		<Container>
-			<h1>This is dev Name</h1>
-			<h3>Open PR Count: {pullsOpened.length}</h3>
-			<h3>Updated PR Count: {pullsUpdated.length}</h3>
-			<h3>Merged PR Count: {pullsMerged.length}</h3>
-		</Container>
-	);
+  useEffect(() => {
+    getOpenPulls();
+    getUpdatedPulls();
+  }, []);
+
+  return (
+    <Container>
+      <h1>This is dev Name</h1>
+      <h3>Open PR Count: {pullsOpened.length}</h3>
+      <h3>Updated PR Count: {pullsUpdated.length}</h3>
+      <h3>Merged PR Count: {pullsMerged.length}</h3>
+      <h3>PR Comment Count: {pullsComments.length}</h3>
+    </Container>
+  );
 };
 
 export default DevReport;
