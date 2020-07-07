411f3f188969debcdfaad15ff6a21f3e7d81ceb1
Rachit Gupta <guptarachit2004@gmail.com>
2020-07-07 18:20:43 +0530

    dashboard skeleton

--- src/About.js
+++ src/About.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const About = () => {
-
+	return (
+		<Container fluid="md">
+			<Row>
+				<Col>1 of 1</Col>
+			</Row>
+		</Container>
+	);
 };
 
 export default About;
--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AuthForm from "./AuthForm";
-import Dashboard from "./Dashboard";
-import About from "./About";
-import DevReport from "./DevReport";
-import { Router } from "@reach/router";
-import { Nav } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthForm from './AuthForm';
+import Dashboard from './Dashboard';
+import About from './About';
+import DevReport from './DevReport';
+import { Router } from '@reach/router';
+import { Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return (
-    <React.StrictMode>
-      <Nav defaultActiveKey="/login" as="ul">
-        <Nav.Item as="li">
-          <Nav.Link href="/home">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/about">About</Nav.Link>
-        </Nav.Item>
-        <Nav.Item as="li">
-          <Nav.Link href="/login">Login</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Router>
-        <AuthForm path="/login" />
-        <Dashboard path="/dashboard" />
-        <About path="/about" />
-        <DevReport path="/report/:id" />
-      </Router>
-    </React.StrictMode>
-  );
+	return (
+		<React.StrictMode>
+			<Nav defaultActiveKey="/login" as="ul">
+				<Nav.Item as="li" className="ml-auto">
+					<Nav.Link href="/about">About</Nav.Link>
+				</Nav.Item>
+				<Nav.Item as="li">
+					<Nav.Link href="/login">Login</Nav.Link>
+				</Nav.Item>
+			</Nav>
+			<Router>
+				<AuthForm path="/login" />
+				<Dashboard path="/dashboard" />
+				<About path="/about" />
+				<DevReport path="/report/:id" />
+			</Router>
+		</React.StrictMode>
+	);
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
