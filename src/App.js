75636c9962ab0f6f1ed27d60c2d8c52c8caa38e4
Chirag Gupta <gchirag@apache.org>
2020-07-09 13:34:48 +0530

    add material-ui and AppBar

--- src/App.js
+++ src/App.js
@@ -5,26 +5,23 @@ import Dashboard from "./Dashboard";
 import About from "./About";
 import DevReport from "./DevReport";
 import { Router } from "@reach/router";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { AppBar, Button, Typography } from "@material-ui/core";
 
 const App = () => {
   return (
     <React.StrictMode>
-      {/*}
-			<Nav defaultActiveKey="/login" as="ul">
-				<Nav.Item as="li" className="ml-auto">
-					<Nav.Link href="/about">About</Nav.Link>
-				</Nav.Item>
-				<Nav.Item as="li">
-					<Nav.Link href="/login">Login</Nav.Link>
-				</Nav.Item>
-			</Nav>
-			<Router>
-				<AuthForm path="/login" />
-				<Dashboard path="/dashboard" />
-				<About path="/about" />
-				<DevReport path="/report/:id" />
-			</Router>*/}
+      <AppBar position="static">
+		  <Typography variant="h6">
+			Sumry
+		  </Typography>
+        <Button color="inherit">Login</Button>
+      </AppBar>
+      <Router>
+        <AuthForm path="/login" />
+        <Dashboard path="/dashboard" />
+        <About path="/about" />
+        <DevReport path="/report/:id" />
+      </Router>
     </React.StrictMode>
   );
 };
