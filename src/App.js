4ba4c7cb1c804fb344278efae920b8b72a3eaf94
Rachit Gupta <guptarachit2004@gmail.com>
2020-07-09 16:58:27 +0530

    add sumry logo + user avatar on app bar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import AuthForm from './AuthForm';
 import Dashboard from './Dashboard';
 import About from './About';
 import { Router } from '@reach/router';
-import { AppBar, Button, Toolbar } from '@material-ui/core';
+import { AppBar, Button, Toolbar, img } from '@material-ui/core';
 
 const App = () => {
 	return (
@@ -13,14 +13,20 @@ const App = () => {
 				<Toolbar>
 					<div className="appBar">
 						<Button color="inherit" className="SumryLogo">
-							Sumry
+							<img alt="" src="https://avatars1.githubusercontent.com/u/38353418?v=4" width="40" />
 						</Button>
-
 						<Button color="inherit" href="/about" className="appBarItem">
 							About Us
 						</Button>
 						<Button color="inherit" href="/login" className="appBarItem">
 							Login
+						</Button>
+						<Button color="inherit" className="SumryLogo">
+							<img
+								alt=""
+								src="https://secure.gravatar.com/avatar/321e8aa99254a96cb5a0ad3d997842c9?d=https%3A%2F%2Favatar-management--avatars.us-west-2.prod.public.atl-paas.net%2Finitials%2FCG-0.png"
+								width="40"
+							/>
 						</Button>
 					</div>
 				</Toolbar>
