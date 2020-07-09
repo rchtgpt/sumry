1380d4714606be516de6a6f8eb051ea33846f1b1
Chirag Gupta <gchirag@apache.org>
2020-07-09 13:21:18 +0530

    Auto stash before merge of "master" and "origin/master"

--- src/DevReport.js
+++ src/DevReport.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import Axios from "axios";
-import { PieChart, Pie, Tooltip, Cell } from "recharts";
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Axios from 'axios';
 
 const DevReport = (props) => {
   const start = new Date();
