61ee22b9417a341f80e3a5b0c8acd57e3771eb4f
Rachit Gupta <guptarachit2004@gmail.com>
2020-07-09 16:09:37 +0530

    hover + form margin-top fix

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AuthForm from "./AuthForm";
-import Dashboard from "./Dashboard";
-import About from "./About";
-import DevReport from "./DevReport";
-import { Router } from "@reach/router";
-import {
-  AppBar,
-  Button,
-  Typography,
-  Toolbar,
-  classes,
-  IconButton,
-  MenuIcon,
-} from "@material-ui/core";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AuthForm from './AuthForm';
+import Dashboard from './Dashboard';
+import About from './About';
+import DevReport from './DevReport';
+import { Router } from '@reach/router';
+import { AppBar, Button, Typography, Toolbar, classes, IconButton, MenuIcon } from '@material-ui/core';
 
 const App = () => {
-  return (
-    <React.StrictMode>
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" href="/about">
-            Sumry
-          </Button>
-          <Button color="inherit" href="/about">
-            About Us
-          </Button>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Router>
-        <AuthForm path="/login" />
-        <Dashboard path="/" />
-        <About path="/about" />
-        <DevReport path="/report/:id" />
-      </Router>
-    </React.StrictMode>
-  );
+	return (
+		<React.StrictMode>
+			<AppBar position="static">
+				<Toolbar>
+					<div className="appBar">
+						<Button color="inherit" href="/dashboard" className="appBarItem">
+							Sumry
+						</Button>
+
+						<Button color="inherit" href="/about" className="appBarItem">
+							About Us
+						</Button>
+						<Button color="inherit" href="/login" className="appBarItem">
+							Login
+						</Button>
+					</div>
+				</Toolbar>
+			</AppBar>
+			<Router>
+				<AuthForm path="/login" />
+				<Dashboard path="/" />
+				<About path="/about" />
+				<DevReport path="/report/:id" />
+			</Router>
+		</React.StrictMode>
+	);
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
