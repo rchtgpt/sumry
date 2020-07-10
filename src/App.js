718f5ebfd1f170f90eb4dd2d7b7931038385f472
Rachit Gupta <guptarachit2004@gmail.com>
2020-07-10 10:06:09 +0530

    final appbar color + logo + card icons

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AuthForm from './AuthForm';
-import Dashboard from './Dashboard';
-import About from './About';
-import { Router } from '@reach/router';
-import { AppBar, Button, Toolbar, img } from '@material-ui/core';
+import React from "react";
+import ReactDOM from "react-dom";
+import AuthForm from "./AuthForm";
+import Dashboard from "./Dashboard";
+import About from "./About";
+import { Router } from "@reach/router";
+import { AppBar, Button, Toolbar, img } from "@material-ui/core";
 
 const App = () => {
-	return (
-		<React.StrictMode>
-			<AppBar position="fixed" color="#0054cf">
-				<Toolbar>
-					<div className="appBar">
-						<Button color="inherit" className="SumryLogo">
-							<img
-								alt=""
-								src="https://user-images.githubusercontent.com/44428198/87115326-83c49880-c290-11ea-8537-b8a305428172.png"
-								width="40"
-							/>
-						</Button>
-						<Button color="inherit" href="/about" className="appBarItem">
-							About Us
-						</Button>
-						<Button color="inherit" href="/login" className="appBarItem">
-							Login
-						</Button>
-					</div>
-				</Toolbar>
-			</AppBar>
-			<Router>
-				<AuthForm path="/login" />
-				<Dashboard path="/" />
-				<About path="/about" />
-			</Router>
-		</React.StrictMode>
-	);
+  return (
+    <React.StrictMode>
+      <AppBar position="fixed" style={{ background: "#2E3B55" }}>
+        <Toolbar>
+          <div className="appBar">
+            <Button color="inherit" className="SumryLogo">
+              <img
+                alt=""
+                src="https://user-images.githubusercontent.com/44428198/87041819-6a7c0780-c210-11ea-906e-8d2bd685d43d.png"
+                width="40"
+              />
+            </Button>
+            <Button color="inherit" href="/about" className="appBarItem">
+              About Us
+            </Button>
+            <Button color="inherit" href="/login" className="appBarItem">
+              Login
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Router>
+        <AuthForm path="/login" />
+        <Dashboard path="/" />
+        <About path="/about" />
+      </Router>
+    </React.StrictMode>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -326,34 +326,98 @@ const Dashboard = (props) => {
           <Grid container item direction="column" spacing={2} sm={9}>
             <Grid container item direction="row" spacing={2}>
               <Grid item sm={3}>
-                <Card style={{ height: "30vh", background: "#03A9F3" }}>
+                <Card
+                  style={{ height: "30vh", background: "#FFFFFF" }}
+                  variant="outlined"
+                >
                   <div className="topStats">
                     <h1>{pullsOpened.length}</h1>
                     <p>Pull Requests Opened</p>
-                    <i className="fa fa-code" />
+                    <svg
+                      width="2em"
+                      height="2em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-box-arrow-in-up"
+                      fill="#2E3B55"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.646 7.854a.5.5 0 0 0 .708 0L8 5.207l2.646 2.647a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 0 .708z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 15a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-1 0v9a.5.5 0 0 0 .5.5z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M1.5 2.5A1.5 1.5 0 0 1 3 1h10a1.5 1.5 0 0 1 1.5 1.5v8A1.5 1.5 0 0 1 13 12h-1.5a.5.5 0 0 1 0-1H13a.5.5 0 0 0 .5-.5v-8A.5.5 0 0 0 13 2H3a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h1.5a.5.5 0 0 1 0 1H3a1.5 1.5 0 0 1-1.5-1.5v-8z"
+                      />
+                    </svg>
                   </div>
                 </Card>
               </Grid>
               <Grid item sm={3}>
-                <Card style={{ height: "30vh", background: "#03A9F3" }}>
+                <Card
+                  style={{ height: "30vh", background: "#FFFFFF" }}
+                  variant="outlined"
+                >
                   <div className="topStats">
                     <h1>{issuesOpened.length}</h1>
                     <p>Issues Opened</p>
-                    <i className="fa fa-code" />
+                    <svg
+                      width="2em"
+                      height="2em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-box-arrow-in-down"
+                      fill="#2E3B55"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.646 8.146a.5.5 0 0 1 .708 0L8 10.793l2.646-2.647a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 0-.708z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 1a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-1 0v-9A.5.5 0 0 1 8 1z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M1.5 13.5A1.5 1.5 0 0 0 3 15h10a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 13 4h-1.5a.5.5 0 0 0 0 1H13a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5v-8A.5.5 0 0 1 3 5h1.5a.5.5 0 0 0 0-1H3a1.5 1.5 0 0 0-1.5 1.5v8z"
+                      />
+                    </svg>
                   </div>
                 </Card>
               </Grid>
               <Grid item sm={3}>
-                <Card style={{ height: "30vh", background: "#03A9F3" }}>
+                <Card
+                  style={{ height: "30vh", background: "#FFFFFF" }}
+                  variant="outlined"
+                >
                   <div className="topStats">
                     <h1>{commitsCreated.length}</h1>
                     <p>Commits Created</p>
-                    <i className="fa fa-code" />
+                    <svg
+                      width="2em"
+                      height="2em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-code-slash"
+                      fill="#2E3B55"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0zm-.999-3.124a.5.5 0 0 1 .33.625l-4 13a.5.5 0 0 1-.955-.294l4-13a.5.5 0 0 1 .625-.33z"
+                      />
+                    </svg>
                   </div>
                 </Card>
               </Grid>
               <Grid item sm={3}>
-                <Card style={{ height: "30vh", background: "#03A9F3" }}>
+                <Card
+                  style={{ height: "30vh", background: "#FFFFFF" }}
+                  variant="outlined"
+                >
                   <div className="topStats">
                     <h1>
                       {pullsComments.length +
@@ -361,7 +425,23 @@ const Dashboard = (props) => {
                         commitComments.length}
                     </h1>
                     <p>Comments</p>
-                    <i className="fa fa-code" />
+                    <svg
+                      width="2em"
+                      height="2em"
+                      viewBox="0 0 16 16"
+                      class="bi bi-chat-left-text"
+                      fill="#2E3B55"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M14 1H2a1 1 0 0 0-1 1v11.586l2-2A2 2 0 0 1 4.414 11H14a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                      />
+                      <path
+                        fill-rule="evenodd"
+                        d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
                   </div>
                 </Card>
               </Grid>
@@ -369,7 +449,10 @@ const Dashboard = (props) => {
 
             <Grid container item direction="row" spacing={2}>
               <Grid item sm={6}>
-                <Card style={{ height: "60vh", background: "purple" }}>
+                <Card
+                  style={{ height: "60vh", background: "#fff" }}
+                  variant="outlined"
+                >
                   <PieChart width={400} height={400}>
                     <Pie
                       dataKey="value"
@@ -393,14 +476,17 @@ const Dashboard = (props) => {
 
           <Grid item container direction="column" sm={3} spacing={2}>
             <Grid item>
-              <Card style={{ height: "45vh", background: "orange" }}>
+              <Card
+                style={{ height: "45vh", background: "orange" }}
+                variant="outlined"
+              >
                 <Col>
                   <CardHeader
                     title="Developers"
                     subheader="September 14, 2016"
                   />
                   {users.map((user) => (
-                    <Card key={user.id} className="devNames">
+                    <Card variant="outlined" key={user.id} className="devNames">
                       <Typography>{user.name}</Typography>
                     </Card>
                   ))}
